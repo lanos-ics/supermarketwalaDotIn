cf25168ea9d052bb569840ef9b48200e48faee0f
lanos-ics <lanos.institute@gmail.com>
2025-09-25 08:59:56 +0530

    Spring Data JPA : learned entity,id,generationType and JpaRepository

--- src/main/java/com/example/supermarketwala/model/Category.java
+++ src/main/java/com/example/supermarketwala/model/Category.java
@@ -1,9 +1,17 @@
 package com.example.supermarketwala.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Category {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long categoryId; 
+	
 	private String categoryName;
 	
 	public Long getCategoryId() {
